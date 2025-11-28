a6ca793dafba770b8d6c32b42e62d37840db0505
Arina <arina.klushnik@gmail.com>
2025-11-29 01:18:13 +0500

    first commit - main,user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
 
+        User[] users = new User[4];
+        users[0] = new User("Амина", "666");
+        users[1] = new User("Арина", "007");
+        users[2] = new User("Богдаша", "123");
+        users[3] = new User("Айгизя", "456");
+
+        System.out.println("Пользователи: ");
+        for (User user : users) {
+            System.out.println(user.toString());
+        }
+
+    }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,3 +1,22 @@
 public class User {
-    String name;
+    private String name;
+    private String id;
+
+    public User(String name, String id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+            return ("Имя: " + getName() + ", ID: " + getId());
+    }
 }
